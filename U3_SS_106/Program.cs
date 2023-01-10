364d7d600f8ee4e01da231bc091c5dcb2ae0e506
BIL-LAB5PC11\Ogrenci <123@123.com>
2023-01-10 15:17:19 +0300

    no message

---
+++ U3_SS_106/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS_106
+{
+    public abstract class Kutupane
+    {
+        public string  kitap { get; set; }
+        public string Dergi { get; set; }
+        public string Ansiklopedi { get; set; }
+
+        public abstract void Oku();
+    }
+
+    public class kitap : Kutupane
+    {
+
+        public override void Oku()
+        {
+            Console.WriteLine("Kitap Oku");
+            Console.WriteLine("Dergi Oku");
+            Console.WriteLine("Ansiklopedi Oku");
+        }   }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                kitap ktp = new kitap
+                {
+                    kitap = "Aşkın Gezegeni"
+                };
+            Console.Write("{0} adlı", ktp.kitap);
+            Console.ReadLine();
+        }
+    }
+}
